d32beffe25ed4d02d984b81916916eff84dec5b1
OASISAsx <wave001133@gmail.com>
2025-01-15 22:36:24 +0700

    carousel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,34 @@
 <template>
-  <SideBar/>
+  <div class="flex justify-center items-center pt-10">
+    <div class="w-[1000px]">
+    <a-carousel autoplay >
+    <div><h3>1</h3></div>
+    <div><h3>2</h3></div>
+    <div><h3>3</h3></div>
+    <div><h3>4</h3></div>
+  </a-carousel>
+  </div>
+  </div>
+ 
  
 </template>
-
-<script>
-import PizZip from 'pizzip'
-import Docxtemplater from 'docxtemplater'
-import JszipUtils from 'jszip-utils'
-import { saveAs } from 'file-saver'
-import ImageModule from 'docxtemplater-image-module-free'
-import SideBar from '@/components/SideBar.vue'
-
-export default {
-  components: {
-    // SideBar
-  },
-  data() {
-    return {
-      img: 'https://media.discordapp.net/attachments/1252284229677744178/1252628911284555919/MG_8493.jpg?ex=6672e915&is=66719795&hm=2524ff49a40149555ad85b6d3aee405e366d92dd1386b042f476f72189fd0781&=&format=webp&width=656&height=437'
-    }
-  },
-  methods: {
-    
-}
-}
+<script setup>
+const onChange = current => {
+  console.log(current);
+};
 </script>
+<style scoped>
 
-<style lang="scss" scoped>
+/* For demo */
+:deep(.slick-slide) {
+  text-align: center;
+  height: 160px;
+  line-height: 160px;
+  background: #364d79;
+  overflow: hidden;
+}
 
+:deep(.slick-slide h3) {
+  color: #fff;
+}
 </style>
